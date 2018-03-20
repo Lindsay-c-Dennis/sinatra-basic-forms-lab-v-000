845a9d08eecc4fdb5a45ff20f12629291828dc50
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-20 18:38:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/' do 
     @name = params[:name]
     @breed = params[:breed]
-    @age = [:age]
+    @age = params[:age]
     @pup = Puppy.new(@name, @breed, @age) 
     erb :display_puppy
   end
